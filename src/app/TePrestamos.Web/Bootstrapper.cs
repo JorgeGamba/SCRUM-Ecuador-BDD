a629d2942b77672d9b4af0d4dfcb7bc4c753d786
JorgeGamba <jorgegamba@gmail.com>
2015-04-18 07:02:05 -0500

    Setting up

---
+++ src/app/TePrestamos.Web/Bootstrapper.cs
@@ -0,0 +1,14 @@
+﻿using Nancy.Conventions;
+using Nancy.Hosting.Aspnet;
+
+namespace TePrestamos.Web
+{
+    public class Bootstrapper : DefaultNancyAspNetBootstrapper
+    {
+        protected override void ConfigureConventions(NancyConventions conventions)
+        {
+            base.ConfigureConventions(conventions);
+            conventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddDirectory("Client", @"Client"));
+        }
+    }
+}
---
+++ src/app/TePrestamos.Web/IndexModule.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.UI.WebControls;
+using Nancy;
+
+namespace TePrestamos.Web
+{
+    public class IndexModule : NancyModule
+    {
+        public IndexModule()
+        {
+            Get["/"] = _ => View["Client/index"];
+        }
+    }
+}
